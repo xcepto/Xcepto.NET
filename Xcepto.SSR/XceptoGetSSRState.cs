328234a69e0de36419d2a23d240b9eb7dc2f9e05
themassiveone <business.daniel.mass@gmail.com>
2026-01-31 10:33:23 +0100

    test: repeat GET when not available yet

--- Xcepto.SSR/XceptoGetSSRState.cs
+++ Xcepto.SSR/XceptoGetSSRState.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Xcepto.Interfaces;
 using Xcepto.States;
 
 namespace Xcepto.SSR
@@ -18,10 +20,15 @@ namespace Xcepto.SSR
 
         private Func<HttpContent,Task<bool>> _responseValidator;
         private Uri _url;
-        private HttpContent _response;
+        private HttpContent? _response;
 
         public override async Task<bool> EvaluateConditionsForTransition(IServiceProvider serviceProvider)
         {
+            if(_response is null)
+                await Execute(serviceProvider);
+            if (_response is null)
+                return false;
+
             if (!await _responseValidator(_response))
                 throw new Exception("response was not validated successfully");
             return true;
@@ -34,19 +41,28 @@ namespace Xcepto.SSR
         
         private async Task Execute(IServiceProvider serviceProvider)
         {
-            HttpClient client = new HttpClient()
+            var loggingProvider = serviceProvider.GetRequiredService<ILoggingProvider>();
+
+            try
             {
-                Timeout = TimeSpan.FromSeconds(1)
-            };
+                HttpClient client = new HttpClient()
+                {
+                    Timeout = TimeSpan.FromSeconds(1)
+                };
 
-            var responseMessage = await client.GetAsync(_url);
+                var responseMessage = await client.GetAsync(_url);
         
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"http request to {_url} faulted");
+                if (!responseMessage.IsSuccessStatusCode)
+                    throw new Exception($"http request to {_url} faulted");
 
-            _response = responseMessage.Content;
-            if (_response == null) 
-                throw new Exception($"http request to {_url} did not return a proper response");
+                _response = responseMessage.Content;
+                if (_response == null) 
+                    throw new Exception($"http request to {_url} did not return a proper response");
+            }
+            catch (Exception e)
+            {
+                loggingProvider.LogDebug(e.Message);
+            }
         }
     }
 }

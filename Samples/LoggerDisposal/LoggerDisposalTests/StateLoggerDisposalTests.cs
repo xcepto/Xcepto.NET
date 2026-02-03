480c4d826c3dc6534dcab1e5ad5156a5c3df4e28
themassiveone <business.daniel.mass@gmail.com>
2026-02-03 08:17:40 +0100

    test: enumerated state disposal

--- Samples/LoggerDisposal/LoggerDisposalTests/StateLoggerDisposalTests.cs
+++ Samples/LoggerDisposal/LoggerDisposalTests/StateLoggerDisposalTests.cs
@@ -3,48 +3,74 @@ using LoggerDisposalTests.Provider;
 using LoggerDisposalTests.Scenarios;
 using LoggerDisposalTests.States;
 using Xcepto;
+using Xcepto.Builder;
+using Xcepto.Strategies.Execution;
+using Xcepto.TestRunner;
 
 namespace LoggerDisposalTests;
 
-[TestFixture]
-public class StateLoggerDisposalTests
+[TestFixture(typeof(AsyncExecutionStrategy))]
+[TestFixture(typeof(EnumeratedExecutionStrategy))]
+public class StateLoggerDisposalTests<T>
+where T: BaseExecutionStrategy, new()
 {
+    public StateLoggerDisposalTests()
+    {
+        _executionStrategy = Activator.CreateInstance<T>();
+        _xceptoTestRunner = new XceptoTestRunner(_executionStrategy);
+    }
+    private string _message = Guid.NewGuid().ToString();
+    private XceptoTestRunner _xceptoTestRunner;
+    private T _executionStrategy;
+
+    private void Run(Action<TransitionBuilder> builder)
+    {
+        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
+        _xceptoTestRunner.Given(new BareMockedScenario(mockedLoggingProvider), 
+            TimeSpan.FromSeconds(5), builder);
+        if (_executionStrategy is AsyncExecutionStrategy asyncExecutionStrategy)
+        {
+            Assert.ThrowsAsync<DisposalInvokingException>(async () =>
+            {
+                await asyncExecutionStrategy.RunAsync();
+            });
+        }
+        else if (_executionStrategy is EnumeratedExecutionStrategy enumeratedExecutionStrategy)
+        {
+            Assert.Throws<DisposalInvokingException>(() =>
+            {
+                EnumeratedTestRunner.RunEnumerator(enumeratedExecutionStrategy.RunEnumerated());   
+            });
+        }
+        
+        Assert.That(mockedLoggingProvider.Flushed(_message));
+    }
+    
+    
     [Test]
     public void StateInitDisposal()
     {
-        string message = Guid.NewGuid().ToString();
-        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
-        var givenTask = XceptoTest.Given(new BareMockedScenario(mockedLoggingProvider), builder =>
+        Run(builder =>
         {
-            builder.AddStep(new InitExceptionState("test state init disposal", message));
+            builder.AddStep(new InitExceptionState("test state init disposal", _message));
         });
-        Assert.ThrowsAsync<DisposalInvokingException>(() => givenTask);
-        Assert.That(mockedLoggingProvider.Flushed(message));
     }
     
     [Test]
     public void StateTransitionDisposal()
     {
-        string message = Guid.NewGuid().ToString();
-        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
-        var givenTask = XceptoTest.Given(new BareMockedScenario(mockedLoggingProvider), builder =>
+        Run(builder =>
         {
-            builder.AddStep(new TransitionExceptionState("test state init disposal", message));
+            builder.AddStep(new TransitionExceptionState("test state init disposal", _message));
         });
-        Assert.ThrowsAsync<DisposalInvokingException>(() => givenTask);
-        Assert.That(mockedLoggingProvider.Flushed(message));
     }
     
     [Test]
     public void StateEnterDisposal()
     {
-        string message = Guid.NewGuid().ToString();
-        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
-        var givenTask = XceptoTest.Given(new BareMockedScenario(mockedLoggingProvider), builder =>
+        Run(builder =>
         {
-            builder.AddStep(new EnterExceptionState("test state init disposal", message));
+            builder.AddStep(new EnterExceptionState("test state init disposal", _message));
         });
-        Assert.ThrowsAsync<DisposalInvokingException>(() => givenTask);
-        Assert.That(mockedLoggingProvider.Flushed(message));
     }
 }

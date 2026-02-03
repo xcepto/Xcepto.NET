6fd754fb0443957d6994fb069c934d93e67b3d3f
themassiveone <business.daniel.mass@gmail.com>
2026-02-03 08:26:56 +0100

    test: enumerated adapter logging exception flush

--- Samples/LoggerDisposal/LoggerDisposalTests/AdapterLoggerDisposalTests.cs
+++ Samples/LoggerDisposal/LoggerDisposalTests/AdapterLoggerDisposalTests.cs
@@ -3,35 +3,64 @@ using LoggerDisposalTests.Exceptions;
 using LoggerDisposalTests.Provider;
 using LoggerDisposalTests.Scenarios;
 using Xcepto;
+using Xcepto.Builder;
+using Xcepto.Strategies.Execution;
+using Xcepto.TestRunner;
 
 namespace LoggerDisposalTests;
 
-[TestFixture]
-public class AdapterLoggerDisposalTests
+[TestFixture(typeof(AsyncExecutionStrategy))]
+[TestFixture(typeof(EnumeratedExecutionStrategy))]
+public class AdapterLoggerDisposalTests<T>
+where T: BaseExecutionStrategy, new()
 {
+    public AdapterLoggerDisposalTests()
+    {
+        _executionStrategy = Activator.CreateInstance<T>();
+        _xceptoTestRunner = new XceptoTestRunner(_executionStrategy);
+    }
+    private string _message = Guid.NewGuid().ToString();
+    private XceptoTestRunner _xceptoTestRunner;
+    private T _executionStrategy;
+
+    private void Run(Action<TransitionBuilder> builder)
+    {
+        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
+        _xceptoTestRunner.Given(new BareMockedScenario(mockedLoggingProvider), 
+            TimeSpan.FromSeconds(5), builder);
+        if (_executionStrategy is AsyncExecutionStrategy asyncExecutionStrategy)
+        {
+            Assert.ThrowsAsync<DisposalInvokingException>(async () =>
+            {
+                await asyncExecutionStrategy.RunAsync();
+            });
+        }
+        else if (_executionStrategy is EnumeratedExecutionStrategy enumeratedExecutionStrategy)
+        {
+            Assert.Throws<DisposalInvokingException>(() =>
+            {
+                EnumeratedTestRunner.RunEnumerator(enumeratedExecutionStrategy.RunEnumerated());   
+            });
+        }
+        
+        Assert.That(mockedLoggingProvider.Flushed(_message));
+    }
+    
     [Test]
     public void InitDisposal()
     {
-        string message = Guid.NewGuid().ToString();
-        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
-        var givenTask = XceptoTest.Given(new BareMockedScenario(mockedLoggingProvider), builder =>
+        Run(builder =>
         {
-            builder.RegisterAdapter(new InitExceptionAdapter(message));
+            builder.RegisterAdapter(new InitExceptionAdapter(_message));
         });
-        Assert.ThrowsAsync<DisposalInvokingException>(() => givenTask);
-        Assert.That(mockedLoggingProvider.Flushed(message));
     }
     
     [Test]
     public void CleanupDisposal()
     {
-        string message = Guid.NewGuid().ToString();
-        MockedLoggingProvider mockedLoggingProvider = new MockedLoggingProvider();
-        var givenTask = XceptoTest.Given(new BareMockedScenario(mockedLoggingProvider), builder =>
+        Run(builder =>
         {
-            builder.RegisterAdapter(new CleanupExceptionAdapter(message));
+            builder.RegisterAdapter(new CleanupExceptionAdapter(_message));
         });
-        Assert.ThrowsAsync<DisposalInvokingException>(() => givenTask);
-        Assert.That(mockedLoggingProvider.Flushed(message));
     }
 }

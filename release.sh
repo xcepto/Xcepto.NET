071887d301405e8f48fdad5c43786bc73dea6c2b
themassiveone <business.daniel.mass@gmail.com>
2026-02-12 19:14:40 +0100

    fix: release script

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ VERSION=$3
 
 dotnet build $PACKAGE -c Release -p:Version=$VERSION
 dotnet pack $PACKAGE -c Release -p:Version=$VERSION -o ./nupkg
-dotnet nuget push ./nupkg/$1.$VERSION.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push ./nupkg/$2.$VERSION.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
